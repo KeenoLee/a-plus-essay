1683fcebe934bb513fc474b5ac4778f89d2bf0ed
Zero Lam <zeroolam@gmail.com>
2022-07-11 17:17:23 +0800

    merge the code from routers/userRoutes.ts to main.ts

--- a_plus_essay_server/main.ts
+++ a_plus_essay_server/main.ts
@@ -1,19 +1,30 @@
-import express from 'express'
-
+import express, { Request, Response } from 'express'
+import { UserService } from './services/UserService'
+import { UserController } from './controllers/UserController'
 import Knex from "knex";
 import config from "./knexfile"
 
-export const knex = Knex(config[process.env.NODE_ENV || "development"])
+export const knex = Knex(config[process.env.NODE_ENV || "development"]);
+const userService = new UserService(knex);
+const userController = new UserController(userService);
 
-const app = express()
+const app = express();
+const userRoutes = express.Router();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.json({ message: 'hello world' })
 })
+
+// userRoutes.post("/signup/student", userController.createStudent);
+// userRoutes.post("/signup/tutor", userController.createTutor);
+// userRoutes.post("/login/password", userController.loginPassword);
+// userRoutes.get("/login/google", userController.loginGoogle);
+// userRoutes.get("/login/facebook", userController.loginFacebook);
+// userRoutes.post("/logout", logout);
 
 const port = 8111
 
