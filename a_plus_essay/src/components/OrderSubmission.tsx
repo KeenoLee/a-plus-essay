52589eb36f4588f1b2a553b3c8253927d2049ef2
tszfungko <tszfungkoktf@gmail.com>
2022-07-17 12:53:43 +0800

    add state for order submission

--- a_plus_essay/src/components/OrderSubmission.tsx
+++ a_plus_essay/src/components/OrderSubmission.tsx
@@ -5,29 +5,29 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import FilePicker from './FilePicker';
 
 export default function OrderSubmission() {
-    const [formValue, SetFormValue] = useState('')
+    const [orderValue, setOrderValue] = useState({})
 
     return (
         <VStack space={4} alignSelf="center" px="4" w={{ base: "100%" }}>
             <Stack>
                 <FormControl>
                     <FormControl.Label>Project Title</FormControl.Label>
-                    <Input variant="underlined" placeholder="" />
+                    <Input variant="underlined" placeholder="" onChangeText={value => setOrderValue({ ...orderValue, title: value })} />
                 </FormControl>
                 <FormControl>
                     <FormControl.Label>Subject</FormControl.Label>
-                    <Input variant="underlined" placeholder="" />
+                    <Input variant="underlined" placeholder="" onChangeText={value => setOrderValue({ ...orderValue, subject: value })} />
                 </FormControl>
             </Stack>
             <HStack>
                 <FormControl.Label>Budget</FormControl.Label>
                 <FormControl w="20">
-                    <Input variant="outline" placeholder="$" />
+                    <Input variant="outline" placeholder="$" onChangeText={value => setOrderValue({ ...orderValue, budget: value })} />
                 </FormControl>
             </HStack>
             <Stack space={4}>
                 <FormControl.Label>Project Description </FormControl.Label>
-                <TextArea h={24} placeholder="Please specify your project requirement (Optional)" w="100%" maxW="300" autoCompleteType={undefined} />
+                <TextArea h={24} placeholder="Please specify your project requirement (Optional)" w="100%" maxW="300" autoCompleteType={undefined} onChangeText={value => setOrderValue({ ...orderValue, description: value })} />
             </Stack>
             <HStack>
                 <FormControl.Label>Guideline</FormControl.Label>
@@ -40,13 +40,17 @@ export default function OrderSubmission() {
             <HStack>
                 <FormControl.Label>Desired Deadline</FormControl.Label>
                 <FormControl w="24">
-                    <DateTimePicker />
+                    <DateTimePicker
+                        onChange={(value: Date) => setOrderValue({ ...orderValue, tutorDeadline: value })}
+                    />
                 </FormControl>
             </HStack>
             <HStack>
                 <FormControl.Label>Actual Deadline</FormControl.Label>
                 <FormControl w="24">
-                    <DateTimePicker />
+                    <DateTimePicker
+                        onChange={(value: Date) => setOrderValue({ ...orderValue, studentDeadline: value })}
+                    />
                 </FormControl>
             </HStack>
             <HStack justifyContent="space-around">
