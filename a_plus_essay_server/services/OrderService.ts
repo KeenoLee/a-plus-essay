75e79b4b035ed9cb8c8cce3eb03e7d56a15d927f
tszfungko <tszfungkoktf@gmail.com>
2022-07-17 17:41:14 +0800

    update order model, add insertion to db for order submission

--- a_plus_essay_server/services/OrderService.ts
+++ a_plus_essay_server/services/OrderService.ts
@@ -8,4 +8,39 @@ export class OrderService {
         const data = await this.knex<OrderItem>("orders").where("students_id", userId)
         return data
     }
+
+    async submitOrderByStudent(order: OrderItem) {
+        const orderId = await this.knex.insert({
+            student_id: 1,
+            title: order.title,
+            grade: order.grade,
+            description: order.description,
+            budget: order.budget,
+            tutor_submission_deadline: order.tutorDeadline,
+            student_submission_deadline: order.studentDeadline
+        }).into('order').returning('id')
+        let subjectId = await this.knex.select('id').from('subject').where('subject_name', order.subject)
+        if (!subjectId) {
+            subjectId = await this.knex.insert({
+                subject_name: order.subject
+            }).into('subject').returning('id')
+        }
+        const orderSubjectId = await this.knex.insert({
+            order_id: orderId,
+            subject_id: subjectId
+        }).into('order_subject').returning('id')
+        order.guidelines.map(async guideline => {
+            await this.knex.insert({
+                order_id: orderId,
+                guideline_base64: guideline.base64
+            })
+        })
+        order.notes.map(async note => {
+            await this.knex.insert({
+                order_id: orderId,
+                note_base64: note.base64
+            })
+        })
+
+    }
 }
