8318243c90a5121556358e542730c83d17f7a8c8
Neo Yeung <dsmkai@gmail.com>
2022-07-17 13:17:48 +0800

    fix bug of react-native-web, added platfrom detection

--- a_plus_essay/babel.config.js
+++ a_plus_essay/babel.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  "presets": ['module:metro-react-native-babel-preset'],
-  "plugins": [
-    ["react-native-web", { "commonjs": true }]
-  ]
-};
+module.exports = function (api) {
+  // This always returns undefined: platform is unavailable outside babel-preset-expo internals?
+  const platform = api.caller((caller) => caller?.platform)
+
+  // Adding 'react-native-web' plugin without a platform check breaks native builds; but platform
+  // check is not possible? So plugins applied here are either "all platforms" or "no platforms"?
+  const plugins = platform === 'web' ? [['react-native-web', { commonjs: true }]] : []
+
+  api.cache(true)
+  return {
+    "presets": ['module:metro-react-native-babel-preset'],
+    plugins
+  }
+}
