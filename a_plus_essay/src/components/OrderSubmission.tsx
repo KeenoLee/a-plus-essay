ac42e475be9bebc9ee121f214fbc0a80ab7eb848
tszfungko <tszfungkoktf@gmail.com>
2022-07-17 15:15:29 +0800

    add date-fns to handle date, add inline style of order submission

--- a_plus_essay/src/components/OrderSubmission.tsx
+++ a_plus_essay/src/components/OrderSubmission.tsx
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
-import { Box, FormControl, Text, Input, Stack, VStack, TextArea, HStack, Button, Icon, CloseIcon, IconButton } from "native-base";
+import { Box, FormControl, Text, Input, Stack, VStack, TextArea, HStack, Button, Icon, CloseIcon, IconButton, View } from "native-base";
 import DateTimePicker from './DateTimePicker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FilePicker from './FilePicker';
+import { format } from 'date-fns'
 
+interface OrderValue {
+    title: string
+    subject: string
+    budget: string
+    description: string | null
+    guideline: string | null
+    note: string | null
+    tutorDeadline: Date | null
+    studentDeadline: Date | null
+}
 export default function OrderSubmission() {
-    const [orderValue, setOrderValue] = useState({})
+    const [orderValue, setOrderValue] = useState<OrderValue>({
+        title: '',
+        subject: '',
+        budget: '',
+        description: null,
+        guideline: null,
+        note: null,
+        tutorDeadline: null,
+        studentDeadline: null
+    })
 
     return (
         <VStack space={4} alignSelf="center" px="4" w={{ base: "100%" }}>
@@ -21,7 +41,7 @@ export default function OrderSubmission() {
             </Stack>
             <HStack>
                 <FormControl.Label>Budget</FormControl.Label>
-                <FormControl w="20">
+                <FormControl w="20" style={{marginLeft: 10}}>
                     <Input variant="outline" placeholder="$" onChangeText={value => setOrderValue({ ...orderValue, budget: value })} />
                 </FormControl>
             </HStack>
@@ -29,29 +49,42 @@ export default function OrderSubmission() {
                 <FormControl.Label>Project Description </FormControl.Label>
                 <TextArea h={24} placeholder="Please specify your project requirement (Optional)" w="100%" maxW="300" autoCompleteType={undefined} onChangeText={value => setOrderValue({ ...orderValue, description: value })} />
             </Stack>
-            <HStack>
-                <FormControl.Label>Guideline</FormControl.Label>
-                <FilePicker />
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                <FormControl.Label style={{ flex: 4 }}>Guideline</FormControl.Label>
+                <View style={{ flex: 3 }}>
+                    <FilePicker />
+                </View>
+                <View style={{ flex: 3 }}></View>
             </HStack>
-            <HStack>
-                <FormControl.Label>Lecture Notes</FormControl.Label>
-                <FilePicker />
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                <FormControl.Label style={{ flex: 4 }}>Lecture Notes</FormControl.Label>
+                <View style={{ flex: 3 }}>
+                    <FilePicker />
+                </View>
+                <View style={{ flex: 3 }}></View>
+
             </HStack>
-            <HStack>
-                <FormControl.Label>Desired Deadline</FormControl.Label>
-                <FormControl w="24">
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                <FormControl.Label style={{ flex: 4 }}>Desired Deadline</FormControl.Label>
+                <FormControl style={{ flex: 3 }}>
                     <DateTimePicker
                         onChange={(value: Date) => setOrderValue({ ...orderValue, tutorDeadline: value })}
                     />
                 </FormControl>
+                {orderValue.tutorDeadline ?
+                    <Text style={{ flex: 3, textAlign:'center' }}>{format(orderValue.tutorDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
+                    <Text style={{ flex: 3 }}></Text>}
             </HStack>
-            <HStack>
-                <FormControl.Label>Actual Deadline</FormControl.Label>
-                <FormControl w="24">
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                <FormControl.Label style={{ flex: 4 }}>Actual Deadline</FormControl.Label>
+                <FormControl style={{ flex: 3 }}>
                     <DateTimePicker
                         onChange={(value: Date) => setOrderValue({ ...orderValue, studentDeadline: value })}
                     />
                 </FormControl>
+                {orderValue.studentDeadline ?
+                    <Text style={{ flex: 3, textAlign:'center' }}>{format(orderValue.studentDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
+                    <Text style={{ flex: 3 }}></Text>}
             </HStack>
             <HStack justifyContent="space-around">
                 <Button>Confirm</Button>
