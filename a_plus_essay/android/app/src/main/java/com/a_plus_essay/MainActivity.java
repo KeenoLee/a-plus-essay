6086970bb4d8bf9bdbda5c8f9157583634908721
Neo Yeung <dsmkai@gmail.com>
2022-07-17 12:47:03 +0800

    modified android version

--- a_plus_essay/android/app/src/main/java/com/a_plus_essay/MainActivity.java
+++ a_plus_essay/android/app/src/main/java/com/a_plus_essay/MainActivity.java
@@ -1,11 +1,19 @@
 package com.a_plus_essay;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
 
+  /**
+   * react-native-screens package requires one additional configuration step to properly work on Android devices.
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
