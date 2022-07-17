3a948b0a99afeafd0ad6ab77bef8d9a83c79ec02
tszfungko <tszfungkoktf@gmail.com>
2022-07-17 16:56:15 +0800

    replace uploading file by uploading image, add function of deleting uploaded image

--- a_plus_essay/src/components/OrderSubmission.tsx
+++ a_plus_essay/src/components/OrderSubmission.tsx
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, FormControl, Text, Input, Stack, VStack, TextArea, HStack, Button, Icon, CloseIcon, IconButton, View } from "native-base";
 import DateTimePicker from './DateTimePicker';
+import { launchImageLibrary } from 'react-native-image-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FilePicker from './FilePicker';
 import { format } from 'date-fns'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 interface Guideline {
-    guideline: string
+    filename: string,
+    base64Data: string
 }
 
 interface Note {
-    note: string
+    filename: string,
+    base64Data: string
 }
 interface OrderValue {
     title: string
     subject: string
     budget: number | string
     description: string | null
-    guideline: Guideline[] | null
-    note: Note[] | null
+    guidelines: Guideline[]
+    notes: Note[]
     tutorDeadline: Date | null
     studentDeadline: Date | null
 }
-
+function shorterFilename(filename: string) {
+    if (filename.length > 10) {
+        return filename.substring(0, 11) + '...'
+    }
+    return filename
+}
 async function fetchOrder(order: OrderValue) {
     const res = await fetch('http://localhost:8111/order-submission', {
         method: 'POST',
@@ -42,12 +51,51 @@ export default function OrderSubmission() {
         subject: '',
         budget: '',
         description: null,
-        guideline: null,
-        note: null,
+        guidelines: [],
+        notes: [],
         tutorDeadline: null,
         studentDeadline: null
     })
 
+    const addGuideline = () => {
+        openGallery(file => {
+            setOrderValue(value => value = {
+                ...value,
+                guidelines: [...value.guidelines, file]
+            })
+
+        })
+    }
+    const addNote = () => {
+        openGallery(file => {
+            setOrderValue(value => value = {
+                ...value,
+                notes: [...value.notes, file]
+            })
+
+        })
+    }
+    const openGallery = (callback: (file: { filename: string, base64Data: string }) => void) => {
+        launchImageLibrary({
+            mediaType: 'photo',
+            includeBase64: true
+        }, (res) => {
+            if (res.didCancel) {
+                console.log('user cancelled image picker')
+            } else if (res.errorMessage) {
+                console.log('Error: ', res.errorMessage)
+            } else {
+                let filename = res.assets?.[0].fileName
+                let base64Data = res.assets?.[0].base64
+                if (filename && base64Data) {
+                    callback({ filename, base64Data })
+                    return
+                }
+                console.log('file is not found')
+                return
+            }
+        })
+    }
     return (
         <VStack space={4} alignSelf="center" px="4" w={{ base: "100%" }}>
             <Stack>
@@ -62,7 +110,7 @@ export default function OrderSubmission() {
             </Stack>
             <HStack>
                 <FormControl.Label>Budget</FormControl.Label>
-                <FormControl w="20" style={{marginLeft: 10}}>
+                <FormControl w="20" style={{ marginLeft: 10 }}>
                     <Input variant="outline" placeholder="$" onChangeText={value => setOrderValue({ ...orderValue, budget: parseInt(value) })} />
                 </FormControl>
             </HStack>
@@ -70,42 +118,90 @@ export default function OrderSubmission() {
                 <FormControl.Label>Project Description </FormControl.Label>
                 <TextArea h={24} placeholder="Please specify your project requirement (Optional)" w="100%" maxW="300" autoCompleteType={undefined} onChangeText={value => setOrderValue({ ...orderValue, description: value })} />
             </Stack>
-            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                <FormControl.Label style={{ flex: 4 }}>Guideline</FormControl.Label>
-                <View style={{ flex: 3 }}>
-                    <FilePicker />
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center', height: 80 }}>
+                <FormControl.Label style={{ flex: 2, justifyContent: 'center' }}>Guideline</FormControl.Label>
+                {/* <FilePicker /> */}
+
+                <View style={{ flex: 4 }}>
+                    {orderValue.guidelines.map((guideline, index) => (
+                        <View key={index} style={{ flexDirection:'row', justifyContent: 'space-between', paddingHorizontal: 25 }}>
+                            <Text style={{ textAlign: 'center' }}>
+                                {shorterFilename(guideline.filename)}
+                            </Text>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setOrderValue(value => value = {
+                                        ...value,
+                                        guidelines: value.guidelines.filter((_, i) => i !== index)
+                                    })
+                                }}
+                            >
+                                <Text>
+                                    x
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
                 </View>
-                <View style={{ flex: 3 }}></View>
-            </HStack>
-            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                <FormControl.Label style={{ flex: 4 }}>Lecture Notes</FormControl.Label>
-                <View style={{ flex: 3 }}>
-                    <FilePicker />
+                <View style={{ flex: 2 }}>
+                    <Button onPress={() => addGuideline()} leftIcon={<Ionicons name="cloud-upload-outline" color="white" />}>
+                        Upload
+                    </Button>
                 </View>
-                <View style={{ flex: 3 }}></View>
 
             </HStack>
-            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center', height: 80 }}>
+                <FormControl.Label style={{ flex: 2, justifyContent: 'center' }}>Lecture Notes</FormControl.Label>
+                <View style={{ flex: 4 }}>
+                    {orderValue.notes.map((note, index) => (
+                        <View key={index} style={{ flexDirection:'row', justifyContent: 'space-between', paddingHorizontal: 25 }}>
+                            <Text style={{ textAlign: 'center' }}>
+                                {shorterFilename(note.filename)}
+                            </Text>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setOrderValue(value => value = {
+                                        ...value,
+                                        notes: value.notes.filter((_, i) => i !== index)
+                                    })
+                                }}
+                            >
+                                <Text>
+                                    x
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Button onPress={() => addNote()} leftIcon={<Ionicons name="cloud-upload-outline" color="white" />}>
+                        Upload
+                    </Button>
+                </View>
+
+
+            </HStack>
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center', height: 50 }}>
                 <FormControl.Label style={{ flex: 4 }}>Desired Deadline</FormControl.Label>
+                {orderValue.tutorDeadline ?
+                    <Text style={{ flex: 3, textAlign: 'center' }}>{format(orderValue.tutorDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
+                    <Text style={{ flex: 3 }}></Text>}
                 <FormControl style={{ flex: 3 }}>
                     <DateTimePicker
                         onChange={(value: Date) => setOrderValue({ ...orderValue, tutorDeadline: value })}
                     />
                 </FormControl>
-                {orderValue.tutorDeadline ?
-                    <Text style={{ flex: 3, textAlign:'center' }}>{format(orderValue.tutorDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
-                    <Text style={{ flex: 3 }}></Text>}
             </HStack>
-            <HStack style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+            <HStack style={{ justifyContent: 'space-between', alignItems: 'center', height: 50 }}>
                 <FormControl.Label style={{ flex: 4 }}>Actual Deadline</FormControl.Label>
+                {orderValue.studentDeadline ?
+                    <Text style={{ flex: 3, textAlign: 'center' }}>{format(orderValue.studentDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
+                    <Text style={{ flex: 3 }}></Text>}
                 <FormControl style={{ flex: 3 }}>
                     <DateTimePicker
                         onChange={(value: Date) => setOrderValue({ ...orderValue, studentDeadline: value })}
                     />
                 </FormControl>
-                {orderValue.studentDeadline ?
-                    <Text style={{ flex: 3, textAlign:'center' }}>{format(orderValue.studentDeadline, 'yyyy-MM-dd HH:mm')}</Text> :
-                    <Text style={{ flex: 3 }}></Text>}
             </HStack>
             <HStack justifyContent="space-evenly">
                 <Button>Confirm</Button>
