41f6a949e35c82a6f06954f869bea9d2273b9d80
tszfungko <tszfungkoktf@gmail.com>
2022-07-18 15:31:11 +0800

    merge changes

--- a_plus_essay_server/seeds/001-create-users.ts
+++ a_plus_essay_server/seeds/001-create-users.ts
@@ -3,6 +3,9 @@ import { hashPassword } from "../utils/hash"
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
+    if ((await knex.schema.hasTable('order'))) {
+        await knex('order').del()
+    }
     await knex("subject").del();
     await knex("sample").del();
     await knex("tutor").del();
@@ -12,9 +15,9 @@ export async function seed(knex: Knex): Promise<void> {
     // Inserts seed entries
     const row =
         await knex("user").insert([
-            { is_admin: false, is_tutor: false, nickname: 'admin', email: 'admin', hashed_password: await hashPassword('admin'), phone_number: 12345678 },
-            { is_admin: false, is_tutor: true, nickname: 'tutor', email: 'tutor', hashed_password: await hashPassword('tutor'), phone_number: 12345679 },
-            { is_admin: true, is_tutor: false, nickname: 'student', email: 'student', hashed_password: await hashPassword('student'), phone_number: 12345688 }
+            { is_admin: false, is_tutor: false, nickname: 'admin', email: 'admin@admin.com', hashed_password: await hashPassword('admin'), phone_number: 12345678 },
+            { is_admin: false, is_tutor: true, nickname: 'tutor', email: 'tutor@tutor.com', hashed_password: await hashPassword('tutor'), phone_number: 12345679 },
+            { is_admin: true, is_tutor: false, nickname: 'student', email: 'student@student.com', hashed_password: await hashPassword('student'), phone_number: 12345688 }
         ]).returning("id");
     let adminId = row[0].id
     let tutorId = row[1].id
@@ -28,6 +31,7 @@ export async function seed(knex: Knex): Promise<void> {
     await knex("tutor").insert([{
         id: tutorId,
         major_id: majorId,
+        student_card_base64: 'student card base 64',
         rating: 5,
         self_intro: 'this is self intro',
         ongoing_order_amount: 0,
