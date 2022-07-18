308e83302de22c338bc7e882f42761f9548a37a1
Kenlee0 <keenoleecw@gmail.com>
2022-07-18 16:28:25 +0800

    added headerStyle

--- a_plus_essay/src/App.tsx
+++ a_plus_essay/src/App.tsx
@@ -46,12 +46,8 @@ import Chatroom from './components/Chatroom';
 
 import OrderStatus from './pages/OrderStatus';
 // import OrderSubmission from './src/components/OrderSubmission';
-<<<<<<< HEAD
-export type RootStackParamList = {
-=======
 
 type RootStackParamList = {
->>>>>>> 5a3d8f8435f905f979e4db42a84896ad95e52219
   Welcome: undefined
   ChatList: undefined
   'Success Register': undefined
@@ -139,21 +135,13 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-<<<<<<< HEAD
-        <Stack.Navigator initialRouteName="Welcome">
-          {/* <Stack.Screen name="Loading" component={LoadingScreen} /> */}
-          <Stack.Screen name="A Plus Company" component={Tabs} />
-          <Stack.Screen name="Welcome" component={LoginPage}/>
-            {/* {(props)=> <LoginPage navigation={props}/>} */}
-=======
-        <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{headerStyle:{backgroundColor: '#ccfbf1'}}}>
           {/* <Stack.Screen name="Loading" component={LoadingScreen} /> */}
           <Stack.Screen name="HomeScreen" component={Tabs} />
           <Stack.Screen name="Welcome" component={LoginPage} />
           {/* {(props)=> <LoginPage navigation={props}/>} */}
->>>>>>> 5a3d8f8435f905f979e4db42a84896ad95e52219
           {/* </Stack.Screen> */}
-          <Stack.Screen name="ChatList" component={ChatList} />
+          <Stack.Screen name="ChatList" component={ChatList}/>
           <Stack.Screen name="Success Register" component={SuccessRegister} />
           <Stack.Screen name="Order Submission" component={OrderSubmission} />
           <Stack.Screen name="Thank You" component={OrderMatched} />
@@ -162,14 +150,8 @@ export default function App() {
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="Chats" component={Chatroom} />
           <Stack.Screen name="Message" component={Notification} />
-<<<<<<< HEAD
-          <Stack.Screen name="Status" component={Status} />
-          {/* <Stack.Screen name="A Plus Company" component={Home} /> */}
-
-=======
->>>>>>> 5a3d8f8435f905f979e4db42a84896ad95e52219
         </Stack.Navigator>
-        <Fabtn />
+        <Fabtn/>
       </NavigationContainer>
     </NativeBaseProvider>
 
