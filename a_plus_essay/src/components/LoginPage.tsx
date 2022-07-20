76b92e0f83841a8df799802f1e1ab4cbc9261d3d
Kenlee0 <102501349+Kenlee0@users.noreply.github.com>
2022-07-20 23:04:32 +0800

    updated style

--- a_plus_essay/src/components/LoginPage.tsx
+++ a_plus_essay/src/components/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
-import { Alert, Image, StyleSheet, Text, TextInput, View } from "react-native"
-import { TouchableOpacity } from "react-native-gesture-handler"
+import { Alert, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from "react-native"
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler"
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from "@react-navigation/core";
 import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -51,76 +51,78 @@ export default function LoginPage({ navigation }: Props) {
     // const navigation = useNavigation()
     return (
         <View>
-            <View style={{marginTop: 30,marginLeft:15}}>
-                <Text style={{ fontWeight: 'bold', fontSize:30, color:"#14532d"}}>Welcome</Text>
-            </View>
-            <View style={{ alignItems: 'center', marginTop: 20}}>
-                {/* <Text style={{ fontWeight: 'bold', fontSize:20, textAlign:'left' }}>Welcome</Text> */}
-                <TextInput style={styles.input} textContentType='emailAddress' autoCapitalize="none" placeholder="Email address" onChangeText={email => setUsername(email)} />
-                <TextInput style={styles.input} textContentType='password' secureTextEntry placeholder="Password" onChangeText={password => setPassword(password)} />
-
-                <View style={{ alignSelf: 'flex-end' }}>
-                    <TouchableOpacity style={{ flexDirection: 'row', padding: 10 }}>
-                        <Text style={{ color: "#14b8a6", fontWeight: 'bold', marginRight: 33 }}>Forgot Password?</Text>
-                    </TouchableOpacity>
+            <ScrollView>
+                <View style={{ marginTop: 20, alignItems: 'center' }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 30, color: "#14532d" }}>Welcome</Text>
                 </View>
+                <View style={{ alignItems: 'center', marginTop: 10 }}>
+                    {/* <Text style={{ fontWeight: 'bold', fontSize:30, color:"#14532d" }}>Welcome</Text> */}
+                    <TextInput style={styles.input} textContentType='emailAddress' autoCapitalize="none" placeholder="Email address" onChangeText={email => setUsername(email)} />
+                    <TextInput style={styles.input} textContentType='password' secureTextEntry placeholder="Password" onChangeText={password => setPassword(password)} />
 
-                <TouchableOpacity style={styles.button} onPress={() => {
-                    // const result = await fetchLogin({ email: username, password: password })
-                    // const result = dispatch({ type: '@@auth/LOGIN_SUCCESS', email: username, password: password })
-                    dispatch(fetchLogin({ email: username, password: password }))
-                    // .unwrap()
-
-                    // result.error ?
-                    //     Alert.alert('Error', result.error) :
-                    //     navigation.navigate('HomeScreen')
-                    // Alert.alert('Success', result)
-                    // Navigate to Home Page here
-                }} >
-                    <Text style={{ textAlign: 'center', color: 'white' ,fontWeight: 'bold'}}>Sign in</Text>
-                </TouchableOpacity>
-
-                <View style={{ flexDirection: 'row', margin: 20 }}>
-                    <Text>Don't have an account? </Text>
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate('Sign up')
-                    }}>
-                        <Text style={{ color: "#14b8a6" ,fontWeight: 'bold'}}>Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-
-
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <View style={{ flexDirection: 'column', flex: 4, borderWidth: 1, borderColor: 'grey', height: 1, margin: 30 }} />
-                    <Text style={{ flex: 1, textAlign: 'center', color: 'grey' }}>
-                        OR
-                    </Text>
-                    <View style={{ flexDirection: 'column', flex: 4, borderWidth: 1, borderColor: 'grey', height: 1, margin: 30 }} />
-                </View>
-
-                <TouchableOpacity style={{ justifyContent: 'center', backgroundColor: "#007AFF", borderColor: 'black', borderWidth: 1, maxWidth: '80%', width: 300, height: 35, borderRadius: 15, margin: 10 }}>
-                    <View style={{}}>
-                        {/* <Image source={ } /> */}
+                    <View style={{ alignSelf: 'flex-end' }}>
+                        <TouchableOpacity style={{ flexDirection: 'row', padding: 10 }}>
+                            <Text style={{ color: "#14b8a6", fontWeight: 'bold', marginRight: 33 }}>Forgot Password?</Text>
+                        </TouchableOpacity>
                     </View>
-                    <Text style={{ textAlign: 'center', color: 'white' }}>Sign in with Facebook</Text>
-                    <View style={{}}></View>
-                </TouchableOpacity>
 
-                <TouchableOpacity style={{ justifyContent: 'center', borderColor: 'black', borderWidth: 1, maxWidth: '80%', width: 300, height: 35, borderRadius: 15, margin: 10 }}>
-                    <View style={{}}>
-                        {/* <Image source={ } /> */}
-                    </View>
-                    <Text style={{ textAlign: 'center' ,fontWeight: 'bold'}}>Sign in with Google</Text>
-                    <View style={{}}></View>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => {
+                        // const result = await fetchLogin({ email: username, password: password })
+                        // const result = dispatch({ type: '@@auth/LOGIN_SUCCESS', email: username, password: password })
+                        dispatch(fetchLogin({ email: username, password: password }))
+                        // .unwrap()
 
-                <View style={{ alignItems: 'center', marginTop: 30 }}>
-                    <Text style={{ fontSize: 12 }}>By using our services you are agreeing to our</Text>
-                    <TouchableOpacity>
-                        <Text style={{ fontWeight: 'bold', fontSize: 12 , fontWeight: 'bold'}}> Terms and Privacy Statement</Text>
+                        // result.error ?
+                        //     Alert.alert('Error', result.error) :
+                        //     navigation.navigate('HomeScreen')
+                        // Alert.alert('Success', result)
+                        // Navigate to Home Page here
+                    }} >
+                        <Text style={{ textAlign: 'center', color: 'white', fontWeight: 'bold' }}>Sign in</Text>
                     </TouchableOpacity>
+
+                    <View style={{ flexDirection: 'row', margin: 20 }}>
+                        <Text>Don't have an account? </Text>
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate('Sign up')
+                        }}>
+                            <Text style={{ color: "#14b8a6", fontWeight: 'bold' }}>Sign Up</Text>
+                        </TouchableOpacity>
+                    </View>
+
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{ flexDirection: 'column', flex: 4, borderWidth: 1, borderColor: 'grey', height: 1, margin: 30 }} />
+                        <Text style={{ flex: 1, textAlign: 'center', color: 'grey' }}>
+                            OR
+                        </Text>
+                        <View style={{ flexDirection: 'column', flex: 4, borderWidth: 1, borderColor: 'grey', height: 1, margin: 30 }} />
+                    </View>
+
+                    <TouchableOpacity style={{ justifyContent: 'center', backgroundColor: "#007AFF", borderColor: 'black', borderWidth: 1, maxWidth: '80%', width: 300, height: 35, borderRadius: 15, margin: 10 }}>
+                        <View style={{}}>
+                            {/* <Image source={ } /> */}
+                        </View>
+                        <Text style={{ textAlign: 'center', color: 'white' }}>Sign in with Facebook</Text>
+                        <View style={{}}></View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ justifyContent: 'center', borderColor: 'black', borderWidth: 1, maxWidth: '80%', width: 300, height: 35, borderRadius: 15, margin: 10 }}>
+                        <View style={{}}>
+                            {/* <Image source={ } /> */}
+                        </View>
+                        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Sign in with Google</Text>
+                        <View style={{}}></View>
+                    </TouchableOpacity>
+
+                    <View style={{ alignItems: 'center', marginTop: 30 }}>
+                        <Text style={{ fontSize: 12 }}>By using our services you are agreeing to our</Text>
+                        <TouchableOpacity>
+                            <Text style={{ fontWeight: 'bold', fontSize: 12 }}> Terms and Privacy Statement</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     )
 }
@@ -132,10 +134,10 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         maxWidth: '80%',
         width: 300,
-        backgroundColor: 'white',
+        backgroundColor: '#F6F6F9',
         textAlign: 'left',
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 3 },
+        shadowOffset: { width: 0, height: 4 },
         shadowOpacity: 0.2,
         shadowRadius: 1,
     },
