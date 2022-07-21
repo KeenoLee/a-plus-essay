80487d6f8ca3cbcffb7ffe36ad4dc65d234e7787
Neo Yeung <dsmkai@gmail.com>
2022-07-21 19:53:59 +0800

    update env files

--- a_plus_essay/src/env/env_sample.ts
+++ a_plus_essay/src/env/env_sample.ts
@@ -1,6 +1,6 @@
 const PORT = 8111
 
 export const env = {
-    BACKEND_URL: `http://localhost:${PORT}`,
+    BACKEND_URL: `http://192.168.168.103:${PORT}`,
     BACKEND_IP: `http://192.168.168.103:${PORT}`
 }
