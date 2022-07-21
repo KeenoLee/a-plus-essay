7e7fb48e8760cbc2de86397bc00786e56911a10d
Neo Yeung <dsmkai@gmail.com>
2022-07-21 16:54:48 +0800

    minor update

--- a_plus_essay_server/seeds/003-create-chat_message.ts
+++ a_plus_essay_server/seeds/003-create-chat_message.ts
@@ -17,7 +17,7 @@ export async function seed(knex: Knex): Promise<void> {
         { id: 3, order_id: orderId1, sent_by_tutor: true, message: 'id1 m3' },//
         { id: 4, order_id: orderId2, sent_by_tutor: false, message: 'id2 m1' },//
         { id: 5, order_id: orderId1, sent_by_tutor: false, message: 'id1 m4' },
-        { id: 6, order_id: orderId2, sent_by_tutor: false, message: 'id2 m2' },//
+        { id: 6, order_id: orderId2, sent_by_tutor: false, message: 'id2 m2 LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONG message' },//
         { id: 7, order_id: orderId2, sent_by_tutor: false, message: 'id2 m3' },
         { id: 8, order_id: orderId3, sent_by_tutor: false, message: 'id3 m1' },
     ]);
@@ -34,7 +34,7 @@ export async function seed(knex: Knex): Promise<void> {
 
     await knex("user_read_message").insert([
         { id: 1, user_id: userId1, order_id: orderId1, last_message_id: 3 },
-        { id: 2, user_id: userId2, order_id: orderId2, last_message_id: 4 },
+        { id: 2, user_id: userId2, order_id: orderId3, last_message_id: 8 },
         { id: 3, user_id: userId3, order_id: orderId2, last_message_id: 6 },
 
     ])
