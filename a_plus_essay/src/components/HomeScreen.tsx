5f4d4b59e3da0311fcecad3c9d17bfd9fde32cf0
Kenlee0 <keenoleecw@gmail.com>
2022-07-22 16:57:58 +0800

    added top card on the Home screen

--- a_plus_essay/src/components/HomeScreen.tsx
+++ a_plus_essay/src/components/HomeScreen.tsx
@@ -1,13 +1,43 @@
 import * as React from "react";
-import { VStack, Center, Text } from 'native-base';
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+import { VStack, Center, Text, Box, HStack, Pressable, Stack, Button } from 'native-base';
+
+
+export function HowToUseCard() {
+    return (
+        // Card Size: Almost cover half of the screen
+        <Box bg="primary.600" alignSelf="center" py="4" mt='4'borderRadius="xl" width="90%" maxWidth="100%" bgColor='tertiary.400'>
+            <Stack>
+                <Stack ml='4'>
+                    {/* Heading Message: 
+                    Tutor: How to start your first Job etc..
+                    Student: How to start your study journey etc.. */}
+                    <Text fontWeight='normal' fontSize='20' color='white'>HOW TO</Text>
+                    <Text fontWeight='bold' fontSize='20' color='white'>Start your study journey</Text>
+                    <Text fontWeight='bold' fontSize='20' color='white'>with our Expert!</Text>
+                </Stack>
+            </Stack>
+            <Stack>
+                {/* Lottle File Animation: Book etc*/}
+            </Stack>
+            <Stack>
+                <Button bgColor='amber.200'>
+                    {/* Button:
+                    Tutor: Let's Started
+                    Student: Start Learning
+                    */}
+                    
+                    {/* onPress -> Show how to use photo etc... */}
+                </Button>
+            </Stack>
+        </Box>
+    )
+}
 
 export default function Home() {
-    // const userInfo = useSelector((state: RootState) => state.auth.user)
 
     return (
-        <VStack space={5} alignItems="flex-end">
+        <VStack space={5}>
+            <HowToUseCard/>
             <Center bg="primary.400" _text={{
                 color: "white",
                 fontWeight: "bold"
