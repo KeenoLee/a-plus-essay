3aa055d980f360b8da2f85c6dd83d948aeb7b02c
tszfungko <tszfungkoktf@gmail.com>
2022-07-23 16:18:19 +0800

    update seed file

--- a_plus_essay/src/components/Account.tsx
+++ a_plus_essay/src/components/Account.tsx
@@ -6,17 +6,26 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useAppNavigation } from '../../routes';
+import TutorBox from './TutorBox';
+import SubjectRow from './SubjectRow';
 
 export default function Account() {
     const state = useSelector((state: RootState) => state.auth)
+    const user = state.user
+    // if (state.tutor) {
+    //     const tutor = state.tutor[0]
+    //     const school = state.tutor[1]
+    //     const transcript = state.tutor[2]
+    // }
     console.log('userInfo in Account', state?.user)
     console.log('tutorInfo in Account', state?.tutor)
-    const navigation = useNavigation()
+    const navigation = useAppNavigation()
     // const [isAuth, setIsAuth] = useState(false)
     const [editProfile, setEditProfile] = useState()
 
     return (
-        state?.user || state?.tutor ?
+        state?.user ?
             <View>
                 <View>
                     <Text>Nickname</Text>
@@ -34,34 +43,42 @@ export default function Account() {
                     <Text>Phone Number</Text>
                     <Text>{state.user?.phone_number}</Text>
                 </View>
-                <View>
-                    <Text>School</Text>
-                    <Text>{state.tutor}</Text>
-                </View>
-                <View>
-                    <Text>Student Card</Text>
-                    <Text>{}</Text>
-                </View>
-                <View>
-                    <Text>Transcript</Text>
-                    <Text>{'Fetch'}</Text>
-                </View>
-                <View>
-                    <Text>Preferred Subjects</Text>
-                    <Text>{'TODO'}</Text>
-                </View>
-                <View>
-                    <Text>Self Introduction</Text>
-                    <Text>{'TODO'}</Text>
-                </View>
-                <View>
-                    <Text>Ongoing Order</Text>
-                    <Text>{'TODO'}</Text>
-                </View>
-                <View>
-                    <Text>Completed Order</Text>
-                    <Text>{'TODO'}</Text>
-                </View>
+                {state?.tutor ?
+                    <>
+                        <View>
+                            <Text>School</Text>
+                            <Text>{state.tutor[1].school}</Text>
+                        </View>
+                        <View>
+                            <Text>Student Card</Text>
+                            <Text>{state.tutor[0].student_card}</Text>
+                        </View>
+                        {state.tutor[2].map((transcript,i) =>
+                            <View key={i}>
+                                <Text>{transcript.filename}</Text>
+                            </View>
+                        )}
+
+                        {/* {state.tutor[3].map(subject =>
+                            <View>
+                                <Text>{subject}</Text>
+                            </View>
+                        )} */}
+
+                        <View>
+                            <Text>Self Introduction</Text>
+                            <Text>{state.tutor[0].self_intro}</Text>
+                        </View>
+                        <View>
+                            <Text>Ongoing Order</Text>
+                            <Text>{state.tutor[0].ongoing_order_amount}</Text>
+                        </View>
+                        <View>
+                            <Text>Completed Order</Text>
+                            <Text>{state.tutor[0].completed_order_amount}</Text>
+                        </View>
+                    </>
+                    : null}
 
             </View> :
 
