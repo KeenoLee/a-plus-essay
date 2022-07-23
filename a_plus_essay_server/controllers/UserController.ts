017552fd90808607304e496f224164cd6b3bfe2c
tszfungko <tszfungkoktf@gmail.com>
2022-07-23 19:37:18 +0800

    add error handling for edit profile

--- a_plus_essay_server/controllers/UserController.ts
+++ a_plus_essay_server/controllers/UserController.ts
@@ -405,8 +405,12 @@ export class UserController {
         try {
             const editInfo = req.body
             console.log('EDIT INFO: ', editInfo)
-            await this.userService.editProfile(editInfo)
-            res.json({ success: true })
+            const result = await this.userService.editProfile(editInfo)
+            if (result.success) {
+                res.json({ success: true })
+                return
+            }
+            return
         } catch (error) {
             console.log(error)
             res.json(error)
