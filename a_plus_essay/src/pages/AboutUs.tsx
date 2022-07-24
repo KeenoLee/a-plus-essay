6eb6a82dd09f22a5a00d2a0335f38e495d73592f
Kenlee0 <keenoleecw@gmail.com>
2022-07-24 18:55:05 +0800

    finished About us Page

--- a_plus_essay/src/pages/AboutUs.tsx
+++ a_plus_essay/src/pages/AboutUs.tsx
@@ -39,12 +39,60 @@ export function AboutUsHeading() {
     )
 }
 
+export function AboutUsBody() {
+    const animationRef = React.useRef<LottieView>(null)
+    return (
+        <Stack marginX='2' mt='4'>
+            <Stack pt='4' alignSelf="center" py="4" mt='4' borderRadius="xl" width="90%" maxWidth="100%" bgColor='cyan.400'>
+                <Stack ml='4'>
+                    <Text textAlign='left' fontWeight='bold' fontSize='25' color='#134e4a'>Core Values</Text>
+                </Stack>
+                <View pt="150">
+                    <LottieView
+                        ref={animationRef}
+                        source={require('../assets/Instant-icon.json')}
+                        autoPlay
+                        loop />
+                </View>
+                <Text mt='2' textAlign='center' fontWeight='bold' fontSize='20' color='#134e4a'>Immediate</Text>
+                <Text mt='2' textAlign='center' fontWeight='medium' fontSize='16' color='white'>Automatically match with</Text>
+                <Text textAlign='center' fontWeight='medium' fontSize='16' color='white'>most suitable tutor </Text>
+            </Stack>
+            <Stack pt='2' alignSelf="center" py="4" mt='4' borderRadius="xl" width="90%" maxWidth="100%" bgColor='lightBlue.400'>
+                <View pt="200" >
+                    <LottieView
+                        ref={animationRef}
+                        source={require('../assets/Quality-icon.json')}
+                        autoPlay
+                        loop />
+                </View>
+                <Text mt='-12' textAlign='center' fontWeight='bold' fontSize='20' color='#134e4a'>Good Quality</Text>
+                <Text mt='2' textAlign='center' fontWeight='medium' fontSize='16' color='white'>Helps over 300 students</Text>
+                <Text textAlign='center' fontWeight='medium' fontSize='16' color='white'>to obtain good result</Text>
+            </Stack>
+            <Stack pt='8' pb='10' alignSelf="center" py="4" mt='4' borderRadius="xl" width="90%" maxWidth="100%" bgColor='blue.400'>
+                <View pt="150">
+                    <LottieView
+                        ref={animationRef}
+                        source={require('../assets/Team-icon.json')}
+                        autoPlay
+                        loop />
+                </View>
+                <Text mt='2' textAlign='center' fontWeight='bold' fontSize='20' color='#134e4a'>Credible</Text>
+                <Text mt='2' textAlign='center' fontWeight='medium' fontSize='16' color='white'>All tutors who have top public</Text>
+                <Text textAlign='center' fontWeight='medium' fontSize='16' color='white'>exam result in Top University</Text>
+            </Stack>
+        </Stack>
+    )
+}
+
 export default function AboutUs() {
     // const animationRef = React.useRef<LottieView>(null)
 
     return (
         <ScrollView>
             <AboutUsHeading />
+            <AboutUsBody />
         </ScrollView>
     )
 }
