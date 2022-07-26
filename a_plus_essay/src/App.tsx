ea4f4b257a337f3126d97bf0722257baa630ebcc
tszfungko <tszfungkoktf@gmail.com>
2022-07-26 11:41:13 +0800

    doing view match order for tutor

--- a_plus_essay/src/App.tsx
+++ a_plus_essay/src/App.tsx
@@ -60,6 +60,7 @@ import { useDispatch } from 'react-redux';
 import jwt_decode from 'jwt-decode'
 import { fetchLoginWithToken } from './redux/auth/actions';
 import { AppDispatch } from './redux/dispatch';
+import ViewMatchedOrder from './pages/ViewMatchedOrder';
 // import OrderSubmission from './src/components/OrderSubmission';
 
 const Stack = createStackNavigator<AppParamList>();
@@ -93,12 +94,14 @@ export const Tabs = () => {
   const state = useSelector((state: RootState) => state.auth);
   return (
     <Tab.Navigator
+      initialRouteName="View Matched Order"
       screenOptions={{
         tabBarActiveTintColor: 'white',
         tabBarStyle: { backgroundColor: '#BBD3CF' },
         headerStyle: { backgroundColor: '#BBD3CF' },
         // headerShown: false
       }}>
+        {/* <Tab.Screen name="View Matched Order" component={ViewMatchedOrder} /> */}
       <Tab.Screen
         name="Home"
         component={HomeScreen}
