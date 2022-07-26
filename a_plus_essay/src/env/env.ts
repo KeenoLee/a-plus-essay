1c16d7b0696202be21455afa16f4ad8e39fcfa79
Kenlee0 <keenoleecw@gmail.com>
2022-07-26 12:52:15 +0800

    updated Tab Screen

--- a_plus_essay/src/env/env.ts
+++ a_plus_essay/src/env/env.ts
@@ -2,5 +2,5 @@ const PORT = 8111
 
 export const env = {
     BACKEND_URL: `http://localhost:${PORT}`,
-    BACKEND_ORIGIN: `http://192.168.168.103:${PORT}`
+    BACKEND_ORIGIN: `http://192.168.168.94:${PORT}`
 }
