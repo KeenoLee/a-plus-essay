63f34ef68ef55eda6adc1a112836435a385c8d68
tszfungko <tszfungkoktf@gmail.com>
2022-07-28 03:22:28 +0800

    try auto re-render order status when order submit

--- a_plus_essay/src/redux/store.ts
+++ a_plus_essay/src/redux/store.ts
@@ -2,13 +2,17 @@ import { authReducer } from "./auth/reducer";
 import { combineReducers, legacy_createStore as createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { AuthState } from "./auth/type";
+import { OrderState } from "./order/type";
+import { orderReducer } from "./order/reducer";
 
 export interface RootState {
     auth: AuthState
+    order: OrderState
 }
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    order: orderReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
