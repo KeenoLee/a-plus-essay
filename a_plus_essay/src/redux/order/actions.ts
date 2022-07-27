44a4774d936a88e930283bc93fb5bc0007d294c8
tszfungko <tszfungkoktf@gmail.com>
2022-07-28 03:22:28 +0800

    try auto re-render order status when order submit

--- a_plus_essay/src/redux/order/actions.ts
+++ a_plus_essay/src/redux/order/actions.ts
@@ -9,9 +9,21 @@ function getChatMessage(orderInfo: OrderInfo, chatMessage: ChatMessage) {
         chatMessage
     }
 }
+export function updateOrder() {
+    return {
+        type: '@@order/UPDATE_ORDER'
+    }
+}
+export function updatedOrder() {
+    return {
+        type: '@@order/UPDATE_ORDER'
+    }
+}
 
 export type OrderActions = 
-ReturnType<typeof getChatMessage>
+ReturnType<typeof getChatMessage> |
+ReturnType<typeof updateOrder> |
+ReturnType<typeof updatedOrder>
 
 export function fetchChatMessage(userId: number, is_Tutor: boolean) {
     return async (dispatch: Dispatch<OrderActions>) => {
